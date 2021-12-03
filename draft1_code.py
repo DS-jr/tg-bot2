c58ac97682be9cd8b4253cb53243a71b0ce4818f
DS-jr <93123740+DS-jr@users.noreply.github.com>
2021-12-03 17:44:20 +0300

    Update draft1_code.py

--- draft1_code.py
+++ draft1_code.py
@@ -1,4 +1,8 @@
 # test code v1:
 q1 = "There are no short cuts to any place worth going. (Beverly Sills)"
 q2 = "Environment always trumps your intentions & power of will."
-print(q1, "\n", q2)
+print(q1, "\n", q2) 
+
+# test code v2 (committed to branch2):
+q3 = "Determination creates a huge internal drive & focus. People feel if you are determined or not. (Lee Kuan Yew)" 
+
